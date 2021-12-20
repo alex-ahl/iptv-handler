066fb8885bca47c964774f79524b08bd1d3f405c
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2021-12-20 16:17:55 +0100

    Refactor to separate crates

---
+++ server/iptv/src/lib.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+pub async fn parser() {
+    let m3u = match env::var("M3U") {
+        Ok(val) => val,
+        Err(e) => e.to_string(),
+    };
+
+    let _res = match get_m3u(&m3u).await {
+        Ok(res) => res,
+        Err(e) => e.to_string(),
+    };
+}
+
+async fn get_m3u(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let res = surf::get(url).await?.body_string().await?;
+    Ok(res)
+}
+
+pub async fn verbs() {
+    let m3u = match env::var("M3U") {
+        Ok(val) => val,
+        Err(e) => e.to_string(),
+    };
+
+    let _res = match get_m3u(&m3u).await {
+        Ok(res) => res,
+        Err(e) => e.to_string(),
+    };
+}
--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -1,9 +1,15 @@
+use api::filters;
 use warp::{log, serve, Filter};
 
-use crate::api;
+#[tokio::main]
+async fn main() {
+    db::connect().await;
+    iptv::parser().await;
+    start_server().await
+}
 
 pub async fn start_server() {
-    let api = api::filters::m3us();
+    let api = filters::m3us();
     let routes = api.with(log("m3us"));
 
     serve(routes).run(([127, 0, 0, 1], 3030)).await;
