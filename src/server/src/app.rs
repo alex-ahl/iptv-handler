83e6d712e122c53fc34451a143285af410479e14
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2022-07-17 13:23:47 +0200

    Init app adjustments

--- src/server/src/app.rs
+++ src/server/src/app.rs
@@ -1,19 +1,46 @@
 use std::sync::Arc;
 
 use api::{handlers::provider::create_provider, models::CreateProviderRequestApiModel};
+use db::services::provider::ProviderApiModel;
 use db::DB;
+use iptv::m3u::builder::create_m3u_file;
+use log::error;
 use url::Url;
+use warp::hyper::{body, StatusCode};
 
 pub async fn init_app(m3u: Url, db: Arc<DB>) {
-    let _res = create_provider(
+    let response = create_provider(
         CreateProviderRequestApiModel {
             name: None::<String>,
             source: m3u.to_string(),
         },
-        db,
+        db.clone(),
     )
     .await
-    .expect("Provider created");
+    .expect("Could not create provider");
 
-    // let test = generate_playlist();
+    let success = StatusCode::from_u16(response.status().as_u16())
+        .unwrap_or_default()
+        .is_success();
+
+    let id = match &body::to_bytes(response.into_body()).await {
+        Ok(res) => serde_json::from_slice::<u64>(res).unwrap_or_default(),
+        Err(_) => {
+            error!("Could not deserialize response body of create provider request");
+            0
+        }
+    };
+
+    if success && id > 0 {
+        let mut provider = ProviderApiModel::new();
+        provider.initialize_db(db);
+
+        if let Ok(provider) = provider.get_provider(id).await {
+            if let Err(err) = create_m3u_file(provider).await {
+                error!(".m3u file created failed with {}", err)
+            }
+        }
+    } else {
+        error!("Could not create provider at this time")
+    }
 }
