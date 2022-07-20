33727513928aa9584cf6a326cbc69fdea91eecbd
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2022-07-20 10:18:57 +0200

    Reworked parser to be async

--- src/iptv/src/m3u/parser.rs
+++ src/iptv/src/m3u/parser.rs
@@ -1,13 +1,9 @@
-use std::{
-    collections::HashMap,
-    io::{BufRead, BufReader, Lines},
-    iter::Skip,
-    slice::Iter,
-};
+use std::collections::HashMap;
 
 use anyhow::Context;
 use log::{debug, error, info};
 use regex::Regex;
+use tokio::io::{AsyncBufReadExt, BufReader, Lines};
 use url::Url;
 
 use super::{
@@ -17,92 +13,90 @@ use super::{
 
 pub async fn parse_m3u_url(url: &Url) -> Result<M3U, anyhow::Error> {
     let m3u = get_m3u(&url).await.context("Could not get M3U content")?;
+    let m3u_reader = BufReader::new(m3u.as_bytes()).lines();
 
-    let m3u = BufReader::new(m3u.as_bytes()).lines();
+    let extinfs = match process_lines(m3u_reader).await {
+        Ok(extinfs) => extinfs,
+        Err(err) => {
+            error!("{}", err);
+            vec![]
+        }
+    };
 
-    let m3u = pre_process(m3u);
-
-    let lines = skip_ext_m3u_line(m3u.iter());
-
-    let parsed_extinf_lines = parse_extinf_entries(lines).to_vec();
-
-    Ok(M3U {
-        extinfs: parsed_extinf_lines,
-    })
+    Ok(M3U { extinfs })
 }
 
-fn pre_process(m3u: Lines<BufReader<&[u8]>>) -> Vec<String> {
-    let array = vec!["#EXTINF", "http", "#EXTM3U"];
+async fn process_lines(mut lines: Lines<BufReader<&[u8]>>) -> Result<Vec<ExtInf>, anyhow::Error> {
+    let mut total_line_count = 0;
+    let mut invalid_line_count = 0;
+    let mut invalid_extinf_entry_count = 0;
 
-    let m3u = m3u.map(|line| line.unwrap()).collect::<Vec<String>>();
-
-    let total_lines = m3u.len();
-
-    let filtered_m3u = m3u
-        .into_iter()
-        .filter(|line| {
-            let is_valid_line = array
-                .iter()
-                .any(|valid_start| line.starts_with(valid_start));
-
-            if !is_valid_line {
-                debug!("Invalid line removed: {}", line);
-            }
-
-            is_valid_line
-        })
-        .collect::<Vec<String>>();
-
-    info!("Removed {} invalid lines", total_lines - filtered_m3u.len());
-
-    filtered_m3u
-}
-
-fn skip_ext_m3u_line(lines: Iter<'_, String>) -> Skip<Iter<String>> {
-    lines.skip(1)
-}
-
-fn parse_extinf_entries(lines: Skip<Iter<String>>) -> Box<Vec<ExtInf>> {
-    let mut parsed_extinf_entries: Vec<ExtInf> = vec![];
     let valid_extinf_line = Regex::new(r#"^(#\S+(?:\s+[^\s="]+=".*")+),(.*)\s*(.*)"#).unwrap();
 
-    let mut lines = lines.enumerate();
+    let mut parsed_extinf_entries: Vec<ExtInf> = vec![];
 
-    while let (Some((.., metadata)), Some((.., url))) = (lines.next(), lines.next()) {
-        let url = Url::parse(&url).context(format!("{}{}", "Could not parse EXTINF url", url));
+    while let Some(line) = lines.next_line().await? {
+        total_line_count += 1;
 
-        match url {
-            Ok(url) if valid_extinf_line.is_match(&metadata) => {
-                let attributes = parse_attributes(&metadata);
-                let group_title = attributes
-                    .get_key_value("group-title")
-                    .map(|value| value.1.clone())
-                    .unwrap_or_default();
+        if !is_valid_line(&line) {
+            invalid_line_count += 1;
+            debug!("Invalid line ignored\n{}", line);
+            continue;
+        }
 
-                parsed_extinf_entries.push(ExtInf {
-                    name: parse_name(&metadata),
-                    attributes,
-                    url: url.clone(),
-                    group_title,
-                });
+        if valid_extinf_line.is_match(&line) {
+            let attributes = parse_attributes(&line);
+            let group_title = get_group_title(&attributes);
 
-                debug!(
-                    "\r\nSuccessfully parsed line channel:\r\n{}\r\n{}",
-                    metadata, url
-                );
+            if let Some(url) = lines.next_line().await.context("Could not read line")? {
+                if let Ok(url) = Url::parse(&url) {
+                    parsed_extinf_entries.push(ExtInf {
+                        name: parse_name(&line),
+                        attributes,
+                        url: url.clone(),
+                        group_title,
+                    });
+
+                    info!("\r\nSuccessfully parsed extinf\r\n{}\r\n{}", line, url);
+                } else {
+                    invalid_extinf_entry_count += 1;
+                    debug!("\nSkipped invalid extinf entry\n{}\n{}", line.as_str(), url);
+                    continue;
+                };
             }
-            Ok(_) => error!("Could not parse line\r\n{}", metadata),
-            Err(e) => error!("Could not parse URL\r\n{}", e),
-        };
+        }
     }
 
-    info!("Parsed {} extinf entries", parsed_extinf_entries.len());
+    log_lines_info(
+        invalid_extinf_entry_count,
+        invalid_line_count,
+        total_line_count,
+    );
 
-    Box::new(parsed_extinf_entries)
+    Ok(parsed_extinf_entries)
+}
+
+fn get_group_title(attributes: &HashMap<String, String>) -> String {
+    attributes
+        .get_key_value("group-title")
+        .map(|value| value.1.clone())
+        .unwrap_or_default()
+}
+
+fn is_valid_line(line: &String) -> bool {
+    let valid_line_starters = vec!["#EXTINF", "#EXTM3U"];
+
+    valid_line_starters
+        .iter()
+        .any(|valid_start| line.starts_with(valid_start))
 }
 
 fn parse_name(extinf_line: &str) -> String {
-    extinf_line.split("\",").last().unwrap().to_string()
+    extinf_line
+        .split("\",")
+        .last()
+        .unwrap_or_default()
+        .to_string()
 }
 
 fn parse_attributes(extinf_line: &str) -> HashMap<String, String> {
@@ -119,4 +113,16 @@ fn parse_attributes(extinf_line: &str) -> HashMap<String, String> {
         .collect();
 
     attributes
+}
+
+fn log_lines_info(invalid_extinf_entry_count: i32, invalid_line_count: i32, total_line_count: i32) {
+    info!(
+        "Ignored {} invalid extinf entries",
+        invalid_extinf_entry_count
+    );
+
+    info!(
+        "Ignored {} invalid lines out of a total of {} lines",
+        invalid_line_count, total_line_count
+    );
 }
