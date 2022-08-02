cf802932d017f0865da8a89c4bda0e5139928921
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2022-08-02 13:32:25 +0200

    Job to purge obsolete m3u files

--- src/server/src/jobs.rs
+++ src/server/src/jobs.rs
@@ -1,8 +1,9 @@
-use std::sync::Arc;
+use std::{ffi::OsStr, path::Path, sync::Arc};
 
 use chrono::Duration;
 use db::DB;
-use log::{debug, error};
+use log::{debug, error, info};
+use tokio::fs;
 use tokio_cron_scheduler::{Job, JobScheduler};
 
 use crate::{app::try_provider_update, environment::Configuration};
@@ -11,10 +12,15 @@ pub(crate) fn init_jobs(config: Configuration, db: Arc<DB>) {
     let schedule = JobScheduler::new().unwrap();
 
     let update_provider_job = create_update_provider_job(config, db);
+    let remove_obsolete_m3u_files = create_remove_obsolete_m3u_files_job();
 
     schedule
         .add(update_provider_job)
-        .expect("Could not add scheduled job");
+        .expect("Could not add update provider job");
+
+    schedule
+        .add(remove_obsolete_m3u_files)
+        .expect("Could not add obsolete m3u files job");
 
     if let Err(e) = schedule.start() {
         error!("Error on scheduler {:?}", e);
@@ -36,7 +42,54 @@ fn create_update_provider_job(config: Configuration, db: Arc<DB>) -> Job {
             })
         },
     )
-    .expect("Could not schedule job");
+    .expect("Could not schedule update provider job");
 
     update_provider_job
+}
+
+fn create_remove_obsolete_m3u_files_job() -> Job {
+    let remove_obsolete_m3u_files =
+        Job::new_repeated_async(Duration::hours(6).to_std().unwrap(), move |_uuid, _l| {
+            Box::pin(async move {
+                debug!("Running delete obsolete m3u files job");
+
+                let mut dir = tokio::fs::read_dir(".").await.unwrap();
+
+                let mut files: Vec<String> = vec![];
+
+                while let Some(file) = dir.next_entry().await.unwrap_or_default() {
+                    let extension = file
+                        .path()
+                        .extension()
+                        .and_then(OsStr::to_str)
+                        .unwrap_or_default()
+                        .to_owned();
+
+                    if extension == "m3u" {
+                        files.push(file.file_name().into_string().unwrap())
+                    }
+                }
+
+                files.sort();
+
+                let number_of_files = files.len();
+                let number_of_files_to_delete = number_of_files - 5;
+
+                let files_to_delete = files
+                    .into_iter()
+                    .take(number_of_files - 5)
+                    .collect::<Vec<String>>();
+
+                for file_name in files_to_delete {
+                    let path = Path::new(&file_name);
+                    fs::remove_file(path).await.unwrap_or_default();
+                    debug!("Removed file {}", file_name);
+                }
+
+                info!("Deleted {} obsolete m3u files", number_of_files_to_delete);
+            })
+        })
+        .expect("Could not schedule delete obsolete m3u files job");
+
+    remove_obsolete_m3u_files
 }
