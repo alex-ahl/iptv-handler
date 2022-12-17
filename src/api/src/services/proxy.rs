01c3888e38f1e792b640ce4463949c2471e083f7
Alexander Åhl <7934262+alexaahl@users.noreply.github.com>
2022-12-17 15:47:00 +0100

    Add headers to stream and attribute proxy

--- src/api/src/services/proxy.rs
+++ src/api/src/services/proxy.rs
@@ -4,6 +4,7 @@ use anyhow::{bail, Context, Error};
 use rest_client::RestClient;
 use url::Url;
 use warp::{
+    http::response::Builder,
     hyper::{Body, Response},
     Reply,
 };
@@ -42,16 +43,15 @@ impl ProxyService {
             let url = Url::parse(&extinf.url)?;
 
             if let Some(ref client) = self.client {
-                let res = client
-                    .get_bytes_stream(&url)
+                let res = client.get(&url).await.context("error on proxy")?;
+                let builder = self.compose_base_response(&res).await?;
+
+                let res = self
+                    .compose_proxy_stream_response(res, builder)
                     .await
-                    .context(format!("Byte stream error for {}", url))?;
+                    .context("error proxying stream")?;
 
-                let body = Body::wrap_stream(res);
-
-                let response = Response::builder().status(200).body(body)?;
-
-                return Ok(response);
+                return Ok(res);
             } else {
                 bail!("Unable to initialize REST client");
             }
@@ -73,16 +73,59 @@ impl ProxyService {
             let url = Url::parse(&attr.value)?;
 
             if let Some(ref client) = self.client {
-                let res = client.get_bytes(&url).await?;
+                let res = client.get(&url).await.context("error on proxy")?;
 
-                let response = Response::builder().status(200).body(res).into_response();
+                let builder = self.compose_base_response(&res).await?;
 
-                return Ok(response);
+                let res = self
+                    .compose_proxy_attribute_response(res, builder)
+                    .await
+                    .context("error proxying attribute")?;
+
+                return Ok(res);
             } else {
                 bail!("Unable to initialize REST client");
             }
         } else {
             bail!("Unable to initialize db");
         }
+    }
+
+    async fn compose_base_response(&self, res: &reqwest::Response) -> Result<Builder, Error> {
+        let status = res.status();
+        let headers = res.headers();
+
+        let mut builder = Response::builder().status(status);
+
+        for (key, val) in headers.iter() {
+            builder = builder.header(key, val);
+        }
+
+        Ok(builder)
+    }
+
+    async fn compose_proxy_stream_response(
+        &self,
+        res: reqwest::Response,
+        response_builder: Builder,
+    ) -> Result<Response<Body>, Error> {
+        let bytes_stream = res.bytes_stream();
+        let body = Body::wrap_stream(bytes_stream);
+
+        let response = response_builder.body(body).into_response();
+
+        Ok(response)
+    }
+
+    async fn compose_proxy_attribute_response(
+        &self,
+        res: reqwest::Response,
+        response_builder: Builder,
+    ) -> Result<Response<Body>, Error> {
+        let bytes = res.bytes().await?;
+
+        let response = response_builder.body(bytes).into_response();
+
+        Ok(response)
     }
 }
